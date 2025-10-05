9d71d7ae19d6c883575bb7ff8b8df93b974243f7
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-10-05 17:11:55 +0200

    Update hook.ts
    I have added for login

--- src/store/hook.ts
+++ src/store/hook.ts
@@ -1,4 +1,6 @@
-import { useDispatch } from "react-redux";
-import type { AppDispatch } from "./store";
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux';
+import type { RootState, AppDispatch } from './store'; 
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
