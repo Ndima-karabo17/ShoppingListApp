560386ca2faeb53698e6b3328b084fd0c2d89057
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-10-07 22:52:57 +0200

    Update store.ts
    I added for grocerylist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import registerReducer from '../features/register/registerSlice'; 
-import loginReducer from '../features/login/loginSlice'
+import loginReducer from '../features/login/loginSlice';
+import groceryReducer from '../features/groceryItem/catelogSlice'; 
 
 export const store = configureStore({
   reducer: {
     register: registerReducer,
-     login: loginReducer,
-  }
+    login: loginReducer,
+    grocery: groceryReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export default store;
