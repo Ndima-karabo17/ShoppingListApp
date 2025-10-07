99d2c7cca84c45a9535e4594c806c3c0d3cd271b
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-10-07 15:32:26 +0200

    Update App.tsx
    I added BrowserRouter, Routes, and Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 //import './App.css'
 
+
+import { BrowserRouter, Route, Routes } from "react-router"
 import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import Home from "./pages/Home"
+import Privacy from "./pages/Privacy"
+import Catelog from "./pages/Catelog"
 
 function App() {
 
 
   return (
     <>
-    <Register />
+    <BrowserRouter>
+   
+      <Routes>
+    <Route path="/" index ></Route>
+    <Route path="/register" element={<Register/>}/>
+<Route path="/login" element={<Login/>}/>
+<Route path="/profile" element={<Profile/>}/>
+<Route path="/home" element= {<Home/>}/>
+<Route path="/catelog" element={<Catelog/>}/>
+<Route path="/privacy" element={<Privacy/>}/>
+      </Routes>
+   
+    </BrowserRouter>
     </>
   )
 }
